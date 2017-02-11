eefb48d5d92a40b5cb46f584f56370cdf508b739
Josh-Harris-47 <joshuajamesharris@gmail.com>
2017-02-11 14:18:56 -0700

    version 2

---
+++ public/app.js
@@ -0,0 +1,30 @@
+var getEmployees = function(){
+  $.ajax({
+    method: "GET",
+    url: "/employees"
+  }).then(function(res){
+    console.log(res);
+    $(".container").empty(card);
+    for(var i = 0; i < res.length; i++){
+      var card = `<div class="card">
+        <h1>`+res[i].name+`</h1>
+        <h3>Email: `+res[i].email+`</h3>
+        <h3>Password: `+res[i].password+`</h3>
+        <h3>id: `+res[i].id+`</h3>
+        <button onclick="deleteEmployees(`+res[i].id+`)">Delete</button>
+        </div>`;
+      $(".container").append(card);
+    }
+  });
+};
+
+getEmployees();
+
+var deleteEmployees = function(id){
+  $.ajax({
+    method: "delete",
+    url: "/employees/" + id,
+  }).then(function(res){
+    getEmployees()
+  })
+}
